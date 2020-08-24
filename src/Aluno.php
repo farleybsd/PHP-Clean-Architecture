a1b3a2f3cd6582c71d0300bcfc12c9190a2c85f4
farleybsd <farley.t.i@hotmail.com>
2020-08-24 15:38:18 -0300

    Adicionando telefones

--- src/Aluno.php
+++ src/Aluno.php
@@ -5,4 +5,10 @@ class Aluno
     private Cpf $cpf;
     private string $nome;
     private Email $email;
+    private array $telefones;
+
+    public function adicionarTelefone(string $ddd,string $numero)
+    {
+        $this->telefones[] = new Telefone($ddd,$numero);
+    }
 }
