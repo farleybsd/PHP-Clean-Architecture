01a6f52f6b6a1f171ff2b7945c2686b1052552ac
farleybsd <farley.t.i@hotmail.com>
2020-08-25 08:24:11 -0300

    Fábricas

---
+++ src/FabricaAlunos.php
@@ -0,0 +1,31 @@
+<?php
+namespace Alura\Arquitetura;
+class FabricaAlunos
+{
+   private Aluno $aluno;
+   public  function comCPFEmailENome(string $numerocpf,string $email,string $nome) : self // retorna a proria instacia da classe
+   {
+       $this->aluno = new Aluno(new CPF($numerocpf),$nome,new Email($email));
+       return $this;
+   }
+
+   public  function adicionaTelefone(string $ddd,string $numero) : self 
+   {
+       $this->aluno->adicionarTelefone($ddd,$numero);
+       return $this;
+   }
+
+   public function  aluno():Aluno
+   {
+       return $this->aluno;
+   }
+
+}
+/* Criando Aluno
+
+$fabrica = new FabricaAlunos();
+$fabrica->comCPFEmailENome('param1','param2','param3')
+->adicionaTelefone('param1','param2')
+->adicionaTelefone('param3','param4')
+->aluno();
+*/
--- src/Indicacao.php
+++ src/Indicacao.php
@@ -4,10 +4,11 @@ class Indicacao
 {
     private Aluno $indicante;
     private Aluno $indicado;
-
+    private \DateTimeImmutable $data;
     public function __construct(Aluno $indicante,Aluno $indicado)
     {
         $this->indicante = $indicante;
         $this->indicado = $indicado;
+        $this->data = new \DateTimeImmutable();
     }
 }
