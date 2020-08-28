13e2953837e919f7872f0fca4458112f805b2f22
farleybsd <farley.t.i@hotmail.com>
2020-08-28 10:36:41 -0300

    Use cases

---
+++ src/Aplicacao/Aluno/MatricularAluno.php
@@ -0,0 +1,23 @@
+<?php
+namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
+
+use Alura\Arquitetura\Dominio\Aluno\RepositorioDeAluno;
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+
+class MatricularAluno
+{
+
+    private RepositorioDeAluno $repositorio;
+
+    public function __construct(RepositorioDeAluno $repositorio)
+    {
+        $this->repositorioDeAluno = $repositorio;
+    }
+
+    public function execulta(MatricularAlunoDto $dados):void
+    {
+        $aluno =Aluno::comCpfNomeEEmail($dados->cpfAluno,$dados->nomeAluno,$dados->emailAluno);
+
+        $this->repositorioDeAluno->adicionar($aluno);
+    }
+}
